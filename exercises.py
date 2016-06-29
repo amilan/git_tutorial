87ae63b40163a97ee13a11e5873bdb988d060592
Jordi Andreu <jandreu@cells.es>
2016-06-29 15:20:03 +0200

    Bug fix in decypher function.

--- exercises.py
+++ exercises.py
@@ -2,21 +2,21 @@ import sys
 import warnings
 
 def test_deprecation_warning():
-  warnings.warn("This is a deprecated function, will disappear", DeprecationWarning)
-  print 'still, the function continues'
+    warnings.warn("This is a deprecated function, will disappear", DeprecationWarning)
+    print 'still, the function continues'
 
 def decypher(s):
-  """Knowing that k -> m, o -> q, e -> g this function
-  decyphers the string s and returns readable text."""
-  return "".join(map(chr, [97+(ord(c)-95)%25 if c.isalpha() else ord(c) for c in s])) 
+    """Knowing that k -> m, o -> q, e -> g this function
+    decyphers the string s and returns readable text."""
+    return "".join(map(chr, [97+(ord(c)-95)%26 if c.isalpha() else ord(c) for c in s]))
 
 def test_decypher():
-  test_string = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle."
-  
-  assert decypher(test_string) == "i hope you didnt translate it by hand. thats what computers are for. doing it in by hand is inefficient and that's why this text is so long."  
+    test_string = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle."
+
+    assert decypher(test_string) == "i hope you didnt translate it by hand. thats what computers are for. doing it in by hand is inefficient and that's why this text is so long."  
 
 def is_vowel(c):
-  pass
+    pass
 
 
 
